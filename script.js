68f3809e1b26562b75f96d9044a37506d966b849
Rhass. <aguilarrhazzhamir@gmail.com>
2024-06-19 21:57:24 +0800

    Update

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ btnfetch.addEventListener('click', async function() {
         if (!response.ok) {
             throw new Error("Invalid Pokemon Name!");
         }
-        const data = await response.json();sss
+        const data = await response.json();
         pokemon(data); 
         Resources(data);
     } catch (error) {
         const errorInput = document.querySelector('.errorInput');
-        errorInput.textContent = error.message;
+        errorInput.textContent = error.message; // Use error.message to get the error description
         errorInput.style.display = 'block';
         errorInput.style.color = 'red';
     }
@@ -26,56 +26,63 @@ function pokemon(data) {
     imgfront.src = pokemonSprite_front;
     imgfront.style.display = "block";
 
-
     const imgback = document.querySelector('#pokemonback');
     imgback.src = pokemonSprite_back;
     imgback.style.display = "block";
 
-    console.log('Sucess!')
+    console.log('Success!');
 }
 
-function Resources(data){
-
+function Resources(data) {
     NameOfPokemon(data);
     Abilities(data);
     TypeofPokemon(data);
 }
 
-function NameOfPokemon(data){
+function NameOfPokemon(data) {
     const speciesName = data.species.name;
     const pokemonName = document.querySelector(".pokemonName");
     pokemonName.textContent = speciesName.toUpperCase();
 }
-function Abilities(data){
-        // abilities
-        const abilities = document.querySelector('.Abilities');
-        abilities.style.display = 'block';
-    
-        // Ability1
-        const abilities1 = data.abilities; 
-        const firstAbility1 = abilities1[0].ability.name; 
+
+function Abilities(data) {
+    const abilities = document.querySelector('.Abilities');
+    abilities.style.display = 'block';
+
+    const abilitiesList = data.abilities;
+
+    // Ability1
+    if (abilitiesList.length > 0) {
+        const firstAbility1 = abilitiesList[0].ability.name;
         const abilityElement1 = document.querySelector('.Ability1');
         abilityElement1.textContent = firstAbility1;
-    
-        // Ability2
-        const abilities2 = data.abilities; 
-        const firstAbility2 = abilities2[1].ability.name; 
+    }
+
+    // Ability2
+    if (abilitiesList.length > 1) {
+        const firstAbility2 = abilitiesList[1].ability.name;
         const abilityElement2 = document.querySelector('.Ability2');
         abilityElement2.textContent = firstAbility2;
+    }
 }
-function TypeofPokemon(data){
-        // Type
-        const Type = document.querySelector('.Type');
-        Type.style.display = 'block';
-    
-        const types1 = data.types;
-        const firstType1 = types1[0].type.name;
+
+function TypeofPokemon(data) {
+    const Type = document.querySelector('.Type');
+    Type.style.display = 'block';
+
+    const typesList = data.types;
+
+    // Type1
+    if (typesList.length > 0) {
+        const firstType1 = typesList[0].type.name;
         const typeElement1 = document.querySelector('.Type1');
-        typeElement1.textContent = firstType1
-    
-        const types2 = data.types;
-        const firstType2 = types2[1].type.name;
+        typeElement1.textContent = firstType1;
+    }
+
+    // Type2
+    if (typesList.length > 1) {
+        const firstType2 = typesList[1].type.name;
         const typeElement2 = document.querySelector('.Type2');
-        typeElement2.textContent = firstType2
-    
+        typeElement2.textContent = firstType2;
+    }
 }
